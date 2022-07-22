6ef7dd73620d0e8ee698817a44e0cd0276649d9b
Alex <alex8224@mail.ru>
2022-07-22 12:21:14 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 0;
+while(number != 0)
+{
+    count = count + (number%10);
+    number = number/10;
+}
+Console.WriteLine($"Ответ: {count}");
+
